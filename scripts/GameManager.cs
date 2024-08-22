55b418868f872cd2bccea81b3f8bc76d0a270d70
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-08-22 11:13:20 +0200

    add basic SceneManager

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -2,8 +2,29 @@ using Godot;
 
 public partial class GameManager : Node {
     public static GameManager Instance { get; private set; }
+    public Node CurrentScene { get; set; }
 
     public override void _Ready() {
         Instance = this;
+
+        // Scene behaviour
+        Viewport root = GetTree().Root;
+        CurrentScene = root.GetChild(root.GetChildCount() - 1);
     }
+
+    #region SceneManager
+
+    public void GotoScene(string path) {
+        CallDeferred(MethodName.DeferredGotoScene, path);
+    }
+
+    private void DeferredGotoScene(string path) {
+        CurrentScene.Free();
+        var nextScene = GD.Load<PackedScene>(path);
+        CurrentScene = nextScene.Instantiate();
+        GetTree().Root.AddChild(CurrentScene);
+        GetTree().CurrentScene = CurrentScene;
+    }
+
+    #endregion
 }
