e38622bdb095735e5e70bb5bab77a3bcac0dc90a
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-08-25 23:22:17 +0200

    wip working visualizer note

--- scripts/NoteVisualizer.cs
+++ scripts/NoteVisualizer.cs
@@ -1,17 +1,26 @@
-using System.Collections.Generic;
 using Godot;
 
 public partial class NoteVisualizer : Node {
-    [Export] private List<Texture2D> _sprites;
+    [Export] private Texture2D[] _sprites;
     [Export] private PackedScene _sprite;
+    [Export] private PackedScene _staff;
     private Note _note;
+
     public override void _Ready() {
         base._Ready();
-        _note = new Note();
+
+        //Test purposes
+        _note = new Note(PitchNotation.G);
+        _note.AddNote(new Note());
+        
+        AddChild(_staff.Instantiate<Sprite2D>());
+        //TODO add special symbol under the staff if C or D
         foreach (var pitch in _note.Pitches) {
             var spriteInstance = _sprite.Instantiate<Sprite2D>();
-            spriteInstance.Texture = _sprites[(int)pitch];
             AddChild(spriteInstance);
+            spriteInstance.Texture = _sprites[(int)_note.Duration];
+            //TODO moving 
+            spriteInstance.Position = (int)pitch * Vector2.Up;
         }
     }
 }
