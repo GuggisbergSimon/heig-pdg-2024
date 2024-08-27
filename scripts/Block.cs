0475c8a16b57b18136b04d5b19b71c530a7f575c
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-08-27 14:02:18 +0200

    add basic block type resource, and source resource

---
+++ scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using Godot;
+
+public enum BlockType {
+    Source,
+    Merger,
+    ShiftUp,
+    ShiftDown,
+    SpeedUp,
+    SpeedDown,
+    Speaker
+}
+
+[GlobalClass]
+public partial class Block : Resource {
+    [Export] public BlockType Type { get; set; }
+    [Export] public Texture2D Sprite { get; set; }
+
+    public Block() : this(BlockType.Source, null) { }
+
+    public Block(BlockType type, Texture2D sprite) {
+        Type = type;
+        Sprite = sprite;
+    }
+}
