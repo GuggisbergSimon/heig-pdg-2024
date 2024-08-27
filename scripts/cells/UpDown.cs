46ef15769633143ecddef0558b9ef9859da82531
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-08-27 15:06:13 +0200

    fixes updown, abstract no longer

--- scripts/cells/UpDown.cs
+++ scripts/cells/UpDown.cs
@@ -1,8 +1,12 @@
 ﻿namespace heigpdg2024.scripts.cells;
 
-public abstract class UpDown : Transit {
+public class UpDown : Transit {
     private bool _isUp;
     public UpDown(Cell input, Cell output, bool isBusy, bool isUp) : base(input, output, isBusy) {
         _isUp = isUp;
+    }
+
+    public override void Process(Note note) {
+        //TODO implement
     }
 }
