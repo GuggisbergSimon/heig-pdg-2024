1dd54e3ef18e4cf14f8554ddb2fea21156b7cb75
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2024-08-30 17:12:31 +0200

    Add ability to change color of note

--- scripts/Duration.cs
+++ scripts/Duration.cs
@@ -12,9 +12,11 @@ public partial class Duration : Resource {
     [Export] public DurationNotation Notation { get; set; }
 
     [Export] public Texture2D Sprite { get; set; }
+    
+    [Export] public Texture2D SpriteWhiteDot { get; set; }
 
     public Duration() : this(DurationNotation.Minim, null) { }
-
+    
     public Duration(DurationNotation notation, Texture2D sprite) {
         Notation = notation;
         Sprite = sprite;
