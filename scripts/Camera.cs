99d45c0171650304ee381a0a440f0872c68ea174
Strueux <43808186+GuggisbergSimon@users.noreply.github.com>
2024-09-03 10:52:23 +0200

    fixes camera speed tied to framerate, adjust speed

--- scripts/Camera.cs
+++ scripts/Camera.cs
@@ -6,57 +6,57 @@ namespace heigpdg2024.scripts.camera;
 /// Class representing a Camera
 /// </summary>
 public partial class Camera : Camera2D {
-    private int MAX_KEYBOARD_SPEED = 10;
-    private int MAX_MOUSE_SPEED = 3;
-    private int MOUSE_BUFFER = 5;
-    private int _velocity_x;
-    private int _velocity_y;
+    [Export] private float _speed = 10f;
+    [Export] private float _mouseBuffer = 5f;
+    private static float MAX_KEYBOARD_SPEED = 10f;
+    private static float MAX_MOUSE_SPEED = 5f;
+    private Vector2 _velocity;
 
     public override void _Process(double delta) {
-        Position += new Vector2(_velocity_x, _velocity_y);
+        Position += (float) delta * _speed * _velocity;
     }
 
     public override void _Input(InputEvent @event) {
         if (Input.IsActionJustPressed("up")) {
-            _velocity_y = -MAX_KEYBOARD_SPEED;
+            _velocity.Y = -MAX_KEYBOARD_SPEED;
         }
         else if (Input.IsActionJustPressed("down")) {
-            _velocity_y = MAX_KEYBOARD_SPEED;
+            _velocity.Y = MAX_KEYBOARD_SPEED;
         }
         else if (Input.IsActionJustPressed("left")) {
-            _velocity_x = -MAX_KEYBOARD_SPEED;
+            _velocity.X = -MAX_KEYBOARD_SPEED;
         }
         else if (Input.IsActionJustPressed("right")) {
-            _velocity_x = MAX_KEYBOARD_SPEED;
+            _velocity.X = MAX_KEYBOARD_SPEED;
         }
         else if (Input.IsActionJustReleased("up") || Input.IsActionJustReleased("down")) {
-            _velocity_y = 0;
+            _velocity.Y = 0;
         }
         else if (Input.IsActionJustReleased("left") || Input.IsActionJustReleased("right")) {
-            _velocity_x = 0;
+            _velocity.X = 0;
         }
         else if (@event is InputEventMouseMotion mouseEvent) {
             Vector2 viewportSize = GetWindow().Size;
             Vector2 mousePosition = mouseEvent.Position;
 
-            if (mousePosition.X <= MOUSE_BUFFER) {
-                _velocity_x = -MAX_MOUSE_SPEED;
+            if (mousePosition.X <= _mouseBuffer) {
+                _velocity.X = -MAX_MOUSE_SPEED;
             }
-            else if (mousePosition.X >= viewportSize.X - MOUSE_BUFFER) {
-                _velocity_x = MAX_MOUSE_SPEED;
+            else if (mousePosition.X >= viewportSize.X - _mouseBuffer) {
+                _velocity.X = MAX_MOUSE_SPEED;
             }
             else {
-                _velocity_x = 0;
+                _velocity.X = 0;
             }
 
-            if (mousePosition.Y <= MOUSE_BUFFER) {
-                _velocity_y = -MAX_MOUSE_SPEED;
+            if (mousePosition.Y <= _mouseBuffer) {
+                _velocity.Y = -MAX_MOUSE_SPEED;
             }
-            else if (mousePosition.Y >= viewportSize.Y - MOUSE_BUFFER) {
-                _velocity_y = MAX_MOUSE_SPEED;
+            else if (mousePosition.Y >= viewportSize.Y - _mouseBuffer) {
+                _velocity.Y = MAX_MOUSE_SPEED;
             }
             else {
-                _velocity_y = 0;
+                _velocity.Y = 0;
             }
         }
     }
